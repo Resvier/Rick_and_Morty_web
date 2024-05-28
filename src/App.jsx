49e4dc9630a4a87c4a26acca76aa728315661a6e
Resvier <elodinelarcano@protonmail.com>
2024-05-28 15:43:24 +0200

    added data fetching method

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Navbar } from './components/Navbar.jsx'
 import { SearchBar } from './components/SearchBar.jsx'
+import { CharacterCard } from './components/CharacterCard.jsx'
 
 const URL_CHARACTERS = 'https://rickandmortyapi.com/api/character/'
 /* const URL_LOCATIONS = 'https://rickandmortyapi.com/api/location'
@@ -9,11 +10,28 @@ const URL_EPISODES = 'https://rickandmortyapi.com/api/episode' */
 function App () {
   const [code, setCode] = useState(1)
   const [search, setSearch] = useState(false)
+  const [character, setCharacter] = useState(null)
+
+  useEffect(() => {
+    if (!code) return
+
+    const ENDPOINT_URL = URL_CHARACTERS + code
+    fetch(ENDPOINT_URL)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data) {
+          setCharacter(data)
+        }
+      }
+
+      )
+  }, [code])
 
   const handleChange = (event) => {
     setCode(event.target.value)
   }
-  const handleKeyPres = (event) => {
+  const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       event.preventDefault()
       setSearch(!search)
@@ -24,8 +42,9 @@ function App () {
     <>
       <Navbar />
       <h1>Character searcher</h1>
-      <SearchBar handleChange={handleChange} handleKeyPres={handleKeyPres} />
+      <SearchBar handleChange={handleChange} handleKeyDown={handleKeyDown} />
       <h3>Character</h3>
+      {search && <CharacterCard character={character} />}
     </>
   )
 }
