201938dd3fa2527331c51ac7125e7d0971d2e84c
Resvier <elodinelarcano@protonmail.com>
2024-05-28 15:12:16 +0200

    added linter and api constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Navbar } from "./components/Navbar.jsx"
-import { SearchBar } from "./components/SearchBar.jsx"
+import { Navbar } from './components/Navbar.jsx'
+import { SearchBar } from './components/SearchBar.jsx'
 
-function App() {
-  
+const URL_CHARACTERS = 'https://rickandmortyapi.com/api/character/'
+/* const URL_LOCATIONS = 'https://rickandmortyapi.com/api/location'
+const URL_EPISODES = 'https://rickandmortyapi.com/api/episode' */
 
+function App () {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <h1>Character searcher</h1>
-        <SearchBar />
-        <h3>Character</h3>
+      <SearchBar />
+      <h3>Character</h3>
     </>
   )
 }
