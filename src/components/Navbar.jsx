dcf51e28af18ef5b097432a896e78f888b632cb5
Resvier <elodinelarcano@protonmail.com>
2024-05-28 15:00:37 +0200

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export function Navbar () {
+  return (
+    <header>
+      <nav className='navbar'>
+        <span className='page-name'>
+          Rick and Morty info
+        </span>
+        <ul className='navbar-list'>
+          <li>Characters</li>
+          <li>Locations</li>
+          <li>Episode</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
